bc88a47df26cce49e7fca576c3a9fe68f5ed4d39
AlexRossa <imabishev2005@gmail.com>
2023-05-26 05:48:45 +0600

    Avengers4

---
+++ js/home.js
@@ -0,0 +1,4 @@
+function greet () {
+    alert ('Welcome!');
+}
+setTimeout (greet, 2000);
---
+++ js/time.js
@@ -0,0 +1,8 @@
+function updateTime() {
+    const currentTime = new Date();
+    const formattedTime = currentTime.toLocaleTimeString();
+
+    document.getElementById('clock').textContent = formattedTime;
+}
+
+setInterval(updateTime, 1000);
